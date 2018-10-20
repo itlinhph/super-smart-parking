17592c53ce3b221d5b0ad709c040cc653e117c56
linkpp <linhphanhust@gmail.com>
2018-10-20 10:09:33 +0700

    filter label

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Thesis-LinhPH
 Thesis - Smart Parking
+
+
+- GreenParking 1750 item
+- BSXM-Google 657 item => Filter => 184 item
+==> Sum = 1934 item
+Generate Character: 11.702 item => Filter => 
--- divideFile.py
+++ divideFile.py
@@ -1,17 +1,38 @@
 import os
 
 # folderImg = "real_chars/"
-folderImg = "real_char2/"
-labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-os.system("mdkir -p labels")
-for label in labels:
-    os.system("mkdir -p labels/"+ label)
+def movieFolder(folderImg):
+    labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-for imgFile in os.listdir(folderImg):
-    name, ext = os.path.splitext(imgFile)
-    nameEnd = name[-1:]
-    command = "mv "+ folderImg+ imgFile + " labels/"+nameEnd
-    os.system(command)
+    os.system("mdkir -p labels")
+    for label in labels:
+        os.system("mkdir -p labels/"+ label)
 
-    print(command)
+    for imgFile in os.listdir(folderImg):
+        name, ext = os.path.splitext(imgFile)
+        nameEnd = name[-1:]
+        command = "mv "+ folderImg+ imgFile + " labels/"+nameEnd
+        os.system(command)
+
+        print(command)
+
+
+def renameCharImg(folderAll):
+    folderLabels = os.listdir(folderAll)
+    print(folderLabels)
+    for folder in folderLabels:
+        listImgs = os.listdir(folderAll+"/"+folder)
+        print(listImgs)
+        count=1
+        for img in listImgs:
+            print img
+            name, ext = os.path.splitext(img)
+            newName = folder +"_" + str(count) + ext
+            os.rename(folderAll+folder+"/"+img,folderAll+folder+"/"+ newName)
+            count+=1
+
+renameCharImg("labels/")
+
+
+# movieFolder("real_chars/")
